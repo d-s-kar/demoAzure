4c2fadf8fd6980136524f3c5fbc21db37f343ee7
deepaksinghkarki <kumaunideepak3@gmail.com>
2024-07-28 08:56:24 +0530

    added

--- src/main/java/com/example/demo/Controller/UseerController.java
+++ src/main/java/com/example/demo/Controller/UseerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UseerController {
 
-    @GetMapping("/getName")
+    @GetMapping("/")
     public String getName() {
         return "Welcome to azure deployment";
     }
